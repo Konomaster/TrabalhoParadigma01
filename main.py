f211ccda0236766bb9317eddee0182d4b0a714e5
Konomaster <thomasdovaletec@gmail.com>
2022-04-22 11:53:45 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Acer
+#
+# Created:     22/04/2022
+# Copyright:   (c) Acer 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#pip instal pyswip
+#pip install openpyxl
+#pip install pandas
+
+import pandas as pd
+from pyswip import Prolog
+
+
+def main():
+    #ambiente de execucao do prolog
+    p=Prolog()
+
+    #usar com index_col=0 tira a primeira coluna
+    dataFrame=pd.read_excel('PlanilhaGastos.xlsx',sheet_name='Pagina1')
+    print(dataFrame.head())
+    excel_rows=[]
+    lista_gastos=[]
+    for i in list(range(0,len(dataFrame.index))):
+        d=dataFrame.iloc[i]
+        d=d.tolist()
+        d[0]=d[0].strftime('%d-%m-%Y')
+        fato='gasto('
+        for j in range(0,len(d)):
+            fato+="'"+str(d[j])
+            if j==len(d)-1:
+                fato+="')"
+            else:
+                fato+="',"
+        print(fato)
+        p.assertz(fato)
+        excel_rows.append(d)
+
+    p.assertz('compramos(Item) :- gasto(_,Item,_,_,_,_,_)')
+    #p.assertz('qtdComprada(Item,Qtd) :- gasto(_,Item,_,_,_,_,_)')
+    #print(list(p.query('gasto(X,Y,Z,A,B,C,D)')))
+    print(bool(list(p.query("compramos('Cimento Campeão CPII')"))))
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ rascunho.pl
@@ -0,0 +1,8 @@
+gasto('01-09-2021','Cimento Campeão CPII','Loja do Zé','20.0','saco','28.0','560.0').
+gasto('01-09-2021','Cimento Campeão CPII','Loja da zica','30.0','saco','28.0','560.0').
+gasto('01-09-2021','Cimento Campeão CPII','Loja do tonho','50.0','saco','28.0','560.0').
+
+compramos(Item) :- gasto(_,Item,_,_,_,_,_).
+
+totalComprado(Item,QtdTotal) :- \+compramos(Item),QtdTotal is 0.
+totalComprado(Item,QtdTotal) :- gasto(_,Item,_,_,_,_,_).
